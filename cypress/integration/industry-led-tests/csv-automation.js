95b086063c1454cd4aebe3b5c386b93713cbbe05
Maciej Fec <maciejfec1996@gmail.com>
2022-04-29 10:07:07 +0100

    added a beforeEach test and started a test to generate csv data file

--- cypress/integration/industry-led-tests/csv-automation.js
+++ cypress/integration/industry-led-tests/csv-automation.js
@@ -1,1 +1,12 @@
 /// <reference types="cypress" />
+
+describe("CSV tool automation", () => {
+    
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/")
+    })
+
+    it("generate CSV data file", () => {
+
+    })
+})
