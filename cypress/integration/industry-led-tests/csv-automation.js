05383c88ed2aa3fd6f1186066f657695340c0285
Maciej Fec <maciejfec1996@gmail.com>
2022-05-01 16:48:25 +0100

    added test to automate the generation and download of csv file to use data in other tests

--- cypress/integration/industry-led-tests/csv-automation.js
+++ cypress/integration/industry-led-tests/csv-automation.js
@@ -7,6 +7,20 @@ describe("CSV tool automation", () => {
     })
 
     it("generate CSV data file", () => {
+        cy.contains("Reset CSV").click()
+        cy.wait(2000)
+        
+        cy.get('#select-key-drop-basic').select('First Name')
+        cy.get('#btn-add-prop').click()
+        cy.get('#select-key-drop-basic').select('Last Name')
+        cy.get('#btn-add-prop').click()
+        cy.get('#select-key-drop-basic').select('Email')
+        cy.get('#btn-add-prop').click()
 
+        cy.get('#entries-input').type("1")
+
+        cy.contains('Generate CSV').click()
+        cy.contains('Arrange CSV').click()
+        cy.contains('Download CSV').click()
     })
 })
