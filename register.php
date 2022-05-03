1d612028075f71f27cb6586e58f81c6f04aab56d
Maciej Fec <maciejfec1996@gmail.com>
2022-05-03 20:53:05 +0100

    added calls to script.js functions

--- register.php
+++ register.php
@@ -20,18 +20,18 @@ require("./functions.php");
             <h1>SIGN UP</h1>
             <form action="<?php registerUser(); ?>" method="post">
                 <p><i id="forename-str" class="fa-solid fa-xmark"></i> Valid forename.</p>
-                <input type="text" name="forename" id="forename" placeholder="Forename">
+                <input type="text" name="forename" id="forename" placeholder="Forename" onkeyup="updateValidForenameIcon()" required>
                 <p><i id="surname-str" class="fa-solid fa-xmark"></i> Valid surname.</p>
-                <input type="text" name="surname" id="surname" placeholder="Surname">
+                <input type="text" name="surname" id="surname" placeholder="Surname" onkeyup="updateValidSurnameIcon()" required>
                 <p><i id="valid-email" class="fa-solid fa-xmark"></i> Valid email address.</p>
-                <input type="email" name="email" id="email" placeholder="Email">
+                <input type="email" name="email" id="email" placeholder="Email" onkeyup="updateValidEmailIcon()" required>
                 <p><i id="pass-length" class="fa-solid fa-xmark"></i> Password is at least 6 characters long.</p>
                 <p><i id="char-upper" class="fa-solid fa-xmark"></i> Password contains at least 1 uppercase character.</p>
                 <p><i id="char-num" class="fa-solid fa-xmark"></i> Password contains at least 1 number.</p>
                 <p><i id="char-special" class="fa-solid fa-xmark"></i> Password contains at least 1 of these special characters (!@#$%^&')</p>
-                <input type="password" name="pass1" id="pass1" placeholder="Password" onkeyup="updateVerifyPasswordIcons()">
+                <input type="password" name="pass1" id="pass1" placeholder="Password" onkeyup="updateVerifyPasswordIcons()" required>
                 <p><i id="pass-match" class="fa-solid fa-xmark"></i> Passwords match.</p>
-                <input type="password" name="pass2" id="pass2" placeholder="Confirm Password" onkeyup="updatePasswordsMatchIcon()">
+                <input type="password" name="pass2" id="pass2" placeholder="Confirm Password" onkeyup="updatePasswordsMatchIcon()" required>
                 <input type="submit" name="submit" id="submit" value="Sign Up">
             </form>
             <p>Already have an account? <a href="#">Sign in here</a></p>
