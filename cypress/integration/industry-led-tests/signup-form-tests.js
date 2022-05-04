5f34add7d051457a0b1279a0c480387d758356c5
Maciej Fec <maciejfec1996@gmail.com>
2022-05-04 14:57:54 +0100

    added tests to check if forename, surname and email fields are validated correctly

--- cypress/integration/industry-led-tests/signup-form-tests.js
+++ cypress/integration/industry-led-tests/signup-form-tests.js
@@ -67,4 +67,106 @@ describe("sign up form tests", () => {
             expect($input[0].validationMessage).to.eq("Please fill out this field.")
         })
     })
+
+    it("checks forename is validated", function () {
+        
+        const badForename = "M@ciej"
+        const badForename2 = "N1c 0l\"'#"
+        
+        // Checks that invalid icon is displayed
+        cy.get("#forename-str").should("have.class", "fa-xmark")
+        cy.get("#forename-str").should("not.have.class", "fa-check")
+
+        // Checks that valid icon is displayed after inputting valid forename
+        cy.get("#forename").type(this.forename)
+        cy.get("#forename-str").should("have.class", "fa-check")
+        cy.get("#forename-str").should("not.have.class", "fa-xmark")
+
+        // Checks that invalid icon is displayed after inputting invalid forename
+        cy.get("#forename").clear()
+        cy.get("#forename").type(badForename)
+        cy.get("#forename-str").should("have.class", "fa-xmark")
+        cy.get("#forename-str").should("not.have.class", "fa-check")
+
+        // Checks that invalid icon is displayed after inputting invalid forename
+        cy.get("#forename").clear()
+        cy.get("#forename").type(badForename2)
+        cy.get("#forename-str").should("have.class", "fa-xmark")
+        cy.get("#forename-str").should("not.have.class", "fa-check")
+    })
+
+    it("checks surname is validated", function() {
+
+        const badSurname = "F3c"
+        const badSurname2 = "M0n t|}ro"
+        
+        // Checks that invalid icon is displayed
+        cy.get("#surname-str").should("have.class", "fa-xmark")
+        cy.get("#surname-str").should("not.have.class", "fa-check")
+
+        // Checks that valid icon is displayed after inputting valid surname
+        cy.get("#surname").type(this.surname)
+        cy.get("#surname-str").should("have.class", "fa-check")
+        cy.get("#surname-str").should("not.have.class", "fa-xmark")
+
+        // Checks that invalid icon is displayed after inputting invalid surname
+        cy.get("#surname").clear()
+        cy.get("#surname").type(badSurname)
+        cy.get("#surname-str").should("have.class", "fa-xmark")
+        cy.get("#surname-str").should("not.have.class", "fa-check")
+
+        // Checks that invalid icon is displayed after inputting invalid surname
+        cy.get("#surname").clear()
+        cy.get("#surname").type(badSurname2)
+        cy.get("#surname-str").should("have.class", "fa-xmark")
+        cy.get("#surname-str").should("not.have.class", "fa-check")
+    })
+
+    it("checks email is validated", function() {
+
+        const badEmail = "maciej.fec.gmail@.com"
+
+        // Checks that invalid icon is displayed
+        cy.get("#valid-email").should("have.class", "fa-xmark")
+        cy.get("#valid-email").should("not.have.class", "fa-check")
+
+        // Checks that valid icon is displayed after inputting valid email
+        cy.get("#email").type(this.email)
+        cy.get("#valid-email").should("have.class", "fa-check")
+        cy.get("#valid-email").should("not.have.class", "fa-xmark")
+        
+        // Checks that invalid icon is displayed after inputting invalid email
+        cy.get("#email").clear()
+        cy.get("#email").type(badEmail)
+        cy.get("#valid-email").should("have.class", "fa-xmark")
+        cy.get("#valid-email").should("not.have.class", "fa-check")
+    })
+
+    it("checks password length is validated", () => {
+
+        const badPass = "pass"
+        const validPass = "abcdef"
+        const validPass2 = "password"
+
+        // Checks that invalid icon is displayed
+        cy.get("#pass-length").should("have.class", "fa-xmark")
+        cy.get("#pass-length").should("not.have.class", "fa-check")
+
+        // Checks if valid icon is displayed when 6 charatcer long password is inputted
+        cy.get("#pass1").type(validPass)
+        cy.get("#pass-length").should("have.class", "fa-check")
+        cy.get("#pass-length").should("not.have.class", "fa-xmark")
+
+        // Checks if valid icon is displayed when password longer than 6 characters is inputted
+        cy.get("#pass1").clear()
+        cy.get("#pass1").type(validPass2)
+        cy.get("#pass-length").should("have.class", "fa-check")
+        cy.get("#pass-length").should("not.have.class", "fa-xmark")
+
+        // Checks if invalid icon is displayed when password less than 6 characters long is inputted
+        cy.get("#pass1").clear()
+        cy.get("#pass1").type(badPass)
+        cy.get("#pass-length").should("have.class", "fa-xmark")
+        cy.get("#pass-length").should("not.have.class", "fa-check")
+    })
 })
