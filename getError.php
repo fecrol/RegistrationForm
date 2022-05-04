4093e413bb9c5bf60ece0dab4505727678196fb6
Maciej Fec <maciejfec1996@gmail.com>
2022-05-04 09:15:34 +0100

    called function to reseet user session data

--- getError.php
+++ getError.php
@@ -1,15 +1,17 @@
 <?php
 
 session_start();
+require("functions.php");
 
 if(isset($_SESSION["error"]) && $_SESSION["error"] == true) {
     $data = ["error" => $_SESSION["error"], "forename" => $_SESSION["data"]["forename"], "surname" => $_SESSION["data"]["surname"], "email" => $_SESSION["data"]["email"]];
     echo json_encode($data);
+    resetSessionData();
 }
 else if (isset($_SESSION["error"]) && $_SESSION["error"] == false) {
     $data = ["error" => $_SESSION["error"]];
-
     echo json_encode($data);
+    resetSessionData();
 }
 else {
     $data = ["error" => null];
