722fa05881b3d39ecce7f423abeea4d1b9306ca1
Maciej Fec <maciejfec1996@gmail.com>
2022-05-16 17:18:54 +0100

    added code which transforms the passed csv data to a javascript object with csv headers as keys.

---
+++ csvToJson.js
@@ -0,0 +1,20 @@
+function csvToJson(data) {
+
+    let userData = {};
+
+    let lines = data.split("\n");
+    let headers = lines[0].split(",");
+    let info = lines[1].split(",");
+
+    for(let i=0; i<headers.length; i++) {
+
+        let key = headers[i].replaceAll("\"", "");
+        let value = info[i].replaceAll("\"", "");
+
+        userData[key] = value;
+    }
+
+    return userData;
+}
+
+export default csvToJson;
